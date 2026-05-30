98518e8d1225a644942b1d5eb760f3e9a1c0b339
Siddhant <siddhant.prasad8@gmail.com>
2026-05-31 03:18:19 +0530

    feat(autonomous-optimization): [Phase 12 Part 4] Export all synthesis, prioritization, forecasting, roadmaps, and recommendations submodules

--- packages/autonomous-optimization/src/index.ts
+++ packages/autonomous-optimization/src/index.ts
@@ -6,3 +6,12 @@ export * from './governance';
 export * from './monitoring';
 export * from './rollback';
 export * from './orchestration';
+export * from './opportunities';
+export * from './prioritization';
+export * from './forecasting';
+export * from './roadmaps';
+export * from './recommendations';
+export * from './memory';
+export * from './timelines';
+export * from './synthesis';
+
